1320aa20f09e6f925fa63511a043abba699d6793
Ilya Volodarsky <ivolo@mit.edu>
2013-10-29 23:24:06 -0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,41 @@
+# health-app
+
+  A simple express subapp for simple load balancer health checks. Returns `200 OK` for `GET /health`. 
+
+## Installation
+
+    $ npm install health-app
+
+## Example
+
+```js
+var express = require('express');
+var Health = require('health-app');
+
+var subapp = new Health();
+
+express()
+  .use('/', subapp.app())
+  .listen(8000);
+```
+
+## API
+
+### Health()
+
+  Generate an express subapp that serves `200 OK for `GET /health`.
+
+## License
+
+```
+WWWWWW||WWWWWW
+ W W W||W W W
+      ||
+    ( OO )__________
+     /  |           \
+    /o o|    MIT     \
+    \___/||_||__||_|| *
+         || ||  || ||
+        _||_|| _||_||
+       (__|__|(__|__|
+```
---
+++ test/index.js
@@ -0,0 +1,29 @@
+
+var assert = require('assert');
+var Health = require('../lib');
+var express = require('express');
+var request = require('supertest');
+
+describe('health-app', function (done) {
+  describe('GET /health', function () {
+    it('should return 200', function (done) {
+      var subapp = new Health().app();
+      var app = express().use('/', subapp);
+      request(app)
+        .get('/health')
+        .expect(200)
+        .end(done);
+    });
+  });
+
+  describe('GET /internal/health', function () {
+    it('should return 200', function (done) {
+      var subapp = new Health().app();
+      var app = express().use('/', subapp);
+      request(app)
+        .get('/internal/health')
+        .expect(200)
+        .end(done);
+    });
+  });
+});
